6f665e203a578c9123c6b747669ccc19ccc093bd
Prageethek <146775637+Prageethek@users.noreply.github.com>
2025-06-03 17:05:31 +0530

    Complete placeOrder and cartTotal components; update cart logic and context functions
    - Finished implementation of PlaceOrder.jsx and CartTotal.jsx
    - Updated Cart.jsx to reflect changes
    - Added updateQuantity and getCartAmount functions in ShopContext.jsx

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 
 export const ShopContext=createContext();
@@ -12,6 +13,7 @@ const ShopContextProvider=(props)=>{
     const[search,setSearch]=useState('');
     const[showSearch,setShowSearch]=useState(false);
     const [cartItems,setCartItems]=useState({})
+    const navigate = useNavigate();
 
     const addToCart= async (itemId,size) =>{
 
@@ -30,6 +32,7 @@ const ShopContextProvider=(props)=>{
             }
         }else{
             cartData[itemId]={}
+
             cartData[itemId][size]=1
         }
 
@@ -51,12 +54,41 @@ const getCartCount=()=>{
     return totalCount;
 }
 
+const updateQuantity =  async (itemId, size, quantity) =>{
+let cartData = structuredClone(cartItems)
+
+cartData[itemId][size] = quantity
+
+setCartItems(cartData);
+
+}
+
+
+const getCartAmount =  ( ) =>{
+    let totalAmount=0
+    for(const items in cartItems){
+        let itemInfo = products.find((product)=> product._id === items)
+        for(const item in cartItems[items]){
+            try{
+
+                if(cartItems[items][item] > 0){
+totalAmount +=itemInfo.price * cartItems[items][item]
+                
+}
+            }catch (error){
+
+            }
+        }
+    }
+    return totalAmount;
+}
 
 
     const value = {
 products,currency,delivery_fee,search,
 setSearch,showSearch,setShowSearch,
-addToCart,cartItems,getCartCount
+addToCart,cartItems,getCartCount,updateQuantity,
+getCartAmount,navigate
     }
 
 
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,10 +2,11 @@ import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title'
 import { assets } from '../assets/frontend_assets/assets'
+import CartTotal from '../components/CartTotal'
 
-export const Cart = () => {
+export const Cart = () => { 
 
-  const {products, currency, cartItems}=useContext(ShopContext)
+  const {products, currency, cartItems, updateQuantity,navigate}=useContext(ShopContext)
   const [cartData,setCartData]=useState([])
 
 useEffect(()=>{
@@ -50,13 +51,22 @@ useEffect(()=>{
   </div>
 </div>
 </div>
-<input className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity}/>
-<img className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt=""/>
+<input  onChange={(e) => e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id,item.size,Number(e.target.value))} className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity}/>
+<img onClick={()=>updateQuantity(item._id,item.size,0)} className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt=""/>
               </div>
             )
           })
         }
       </div>
+
+<div className='flex justify-end my-20'>
+<div className='w-full sm:w-[450px]'>
+<CartTotal/>
+<div className='w-full text-end'>
+<button onClick={()=>(navigate('/place-order'))} className='bg-black text-white px-8 py-3 my-10 cursor-pointer'>PROCEED TO CHECKOUT</button>
+</div>
+</div>
+</div>
 
     </div>
   )
