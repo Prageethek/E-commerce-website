ad60e65085788aafcb5fec7e35cd272491018eea
Prageethek <146775637+Prageethek@users.noreply.github.com>
2025-06-27 00:28:43 +0530

    fix a bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import OurPolicy from '../components/OurPolicy'
-import NewsLetterBox from '../components/NewsLetterBox'
+import NewsLetterBox from '../components/NewsLetterBox.jsx'
 import BestSeller from '../components/BestSeller.jsx'
 
 export const Home = () => {
